45f8038ee4ba4882cafd4a5743f8058fea126e97
RomanWuattier <roman.wuattier@gmail.com>
2019-12-04 22:48:28 +0100

    Day 4: Secure Container - Split part 1 and 2

--- 04/secure_container.py
+++ 04/secure_container.py
@@ -1,24 +1,36 @@
 puzzle_input = '367479-893698'    
 
-def is_password(num):
-    digits = list(int(d) for d in str(num))
-    prev = 0
-    group = 0
-    is_adj = False
-    for d in digits:
-        if d < prev:
-            return False
-        elif d == prev:
-            group += 1
-        else:
-            if group == 1:
-                is_adj = True
-            group = 0
 
-        prev = d
-    return is_adj or group == 1
+def is_increasing(num):
+    digits = list(str(num))
+    for i in range(len(digits) - 1):
+        if digits[i] > digits[i + 1]:
+            return False
+    return True
+
+
+def has_double(num):
+    digits = list(str(num))
+    return len(set(digits)) < len(digits)
+
+
+def group(num):
+    digits = list(str(num))
+    count = 0
+    for i in range(len(digits) - 1):
+        if digits[i] == digits[i + 1]:
+            count += 1
+        elif count != 1:
+            count = 0
+        else:
+            return True
+    return False or count == 1
 
 
 if __name__ == '__main__':
     minimum, maximun = puzzle_input.split('-')
-    print(len([num for num in range(int(minimum), int(maximun)) if (is_password(num))]))
+    arr = [num for num in range(int(minimum), int(maximun)) if is_increasing(num) and has_double(num)]
+    # Part 1
+    print(len(arr))
+    # Part 2
+    print(len([a for a in arr if group(a)]))
